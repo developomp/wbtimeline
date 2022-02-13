f4dff526be49b0cfc680532573486cb4377e5f11
developomp <developomp@gmail.com>
2022-02-13 10:17:43 +0900

    updated data struct - and moved date from rust file to markdown

--- src/app.rs
+++ src/app.rs
@@ -40,6 +40,7 @@ pub fn app() -> Html {
                title: data.title.to_string(),
                description: data.description.to_string(),
                category: data.category.to_string(),
+               subcategory: data.subcategory.to_string(),
                media: None,
                button: None,
            }} />
@@ -51,22 +52,14 @@ pub fn app() -> Html {
             <Header />
 
             <div class="timeline">
-                <Entry data={Data {
-                    date: "Nov 13, 2017".to_string(),
-                    title: "Release of open beta for browsers".to_string(),
-                    description: "The grand beginning of the legacy.".to_string(),
-                    category: "development".to_string(),
-                    media: None,
-                    button: None,
-                }} />
-
                 { for entries }
 
                 <Entry data={Data {
                     date: "NOW".to_string(),
-                    title: "You can be part of WB history".to_string(),
-                    description: "Join the War Brokers discord community and be a part of War Brokers' glorious history!".to_string(),
+                    title: "You can be part of the history".to_string(),
+                    description: "Join the War Brokers discord community today and be a part of the history!".to_string(),
                     category: "community".to_string(),
+                    subcategory: "".to_string(),
                     media: None,
                     button: Some(["https://discord.gg/warbrokers".to_string(), "War Brokers discord server".to_string()]),
                 }} />
--- src/data.rs
+++ src/data.rs
@@ -2,10 +2,11 @@ use serde::Deserialize;
 
 #[derive(Clone, PartialEq, Deserialize, Debug)]
 pub struct Data {
-    pub title: String,
     pub date: String,
+    pub title: String,
     pub description: String,
-    pub media: Option<Vec<String>>,
     pub category: String,
+    pub subcategory: String,
+    pub media: Option<Vec<String>>,
     pub button: Option<[String; 2]>,
 }
